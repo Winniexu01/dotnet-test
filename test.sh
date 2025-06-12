b7ead48756f3d6871ff5956bef9ede449fb1dd64
Winniexu01 <v-wexu@microsoft.com>
2025-06-12 22:50:17 +0800

    Update

--- test.sh
+++ test.sh
@@ -1,13 +1,16 @@
 set -euo pipefail
 
-baseXmlPath="/root/local-git-backup/MergedManifest.xml"
+baseXmlPath="/root/local-git/MergedManifest.xml"
 
-nodes=$(xmllint --xpath "string(//Blob[contains(@Id, 'Sdk') and contains(@Id, '-linux') and contains(@Id, '-x64.tar.gz')])" "$baseXmlPath")
+nodes=$(xmllint --xpath '//Blob[contains(@Id, "dotnet-sdk") and contains(@Id, "linux") and contains(@Id, "x64.tar.gz")]' "$baseXmlPath")
+
 
 while read -r node; do
-    name=$(echo "$node" | xmllint --xpath "string(//@PipelineArtifactName)" -)
-    path=$(echo "$node" | xmllint --xpath "string(//@PipelineArtifactPath)" -)
+    name=$(echo "$node" | xmllint --xpath 'string(//@PipelineArtifactName)' -)
+    path=$(echo "$node" | xmllint --xpath 'string(//@PipelineArtifactPath)' -)
 
-    echo "Artifact Name: $name"
-    echo "Artifact Path: $path"
+    if [[ "$path" =~ dotnet-sdk-[0-9]+\.[0-9]+\.[0-9]+(-?(alpha|preview|rc|rtm)[0-9\.\-]*)?-linux.*-x64\.tar\.gz$ ]]; then
+        echo "Artifact Name: $name"
+        echo "Artifact Path: $path"
+    fi
 done <<< "$nodes"
